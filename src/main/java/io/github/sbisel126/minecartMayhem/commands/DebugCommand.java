7b33d9e09228af864fe00fc6290f5823e0fd76af
Andrew Goodwin <pgoodwi3@students.kennesaw.edu>
2025-04-09 11:41:42 -0400

    remove debug content from debugcommand

--- src/main/java/io/github/sbisel126/minecartMayhem/commands/DebugCommand.java
+++ src/main/java/io/github/sbisel126/minecartMayhem/commands/DebugCommand.java
@@ -2,20 +2,14 @@ package io.github.sbisel126.minecartMayhem.commands;
 
 import io.github.sbisel126.minecartMayhem.MinecartMayhem;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.title.Title;
 import org.bukkit.Bukkit;
-import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 
-import java.time.Duration;
-import java.util.jar.Attributes;
 
 public class DebugCommand implements Listener, CommandExecutor {
     MinecartMayhem plugin;
@@ -27,10 +21,6 @@ public class DebugCommand implements Listener, CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
-        if (!(sender instanceof Player player)) {
-            sender.sendMessage("Only players can execute this command.");
-            return true;
-        }
         sender.sendMessage(Component.text("wow"));
         return true;
     }
