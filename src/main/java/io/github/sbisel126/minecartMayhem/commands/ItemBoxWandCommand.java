487e20e3b3fa7f4c407a8dc77695bce4837991bb
fabzxz <144719878+fabzxz@users.noreply.github.com>
2025-04-18 02:05:52 -0400

    Update ItemBoxWandCommand.java

--- src/main/java/io/github/sbisel126/minecartMayhem/commands/ItemBoxWandCommand.java
+++ src/main/java/io/github/sbisel126/minecartMayhem/commands/ItemBoxWandCommand.java
@@ -1,1 +1,33 @@
+package io.github.sbisel126.minecartMayhem.commands;
+
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.ChatColor;
+import org.jetbrains.annotations.NotNull;
+
+public class ItemBoxWandCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("Only players can use this command.");
+            return true;
+        }
+
+        ItemStack wand = new ItemStack(Material.STICK);
+        ItemMeta meta = wand.getItemMeta();
+        if (meta != null) {
+            meta.setDisplayName(ChatColor.GOLD + "Item Box Wand");
+            wand.setItemMeta(meta);
+        }
+
+        player.getInventory().addItem(wand);
+        player.sendMessage(ChatColor.GREEN + "You received the Item Box Wand!");
+        return true;
+    }
+}
 
